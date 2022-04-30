46c3fad96ce3c3fce38c7cc64ce7c08c4b4071bf
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-04-30 06:07:34 -0400

    Update hardhat.config.js

--- test/integration/hardhat.config.js
+++ test/integration/hardhat.config.js
@@ -21,7 +21,7 @@ module.exports = {
   solidity: {
     compilers: [{version: "0.8.0"},
     {version: "0.8.7"},
-    {version: "0.8.12"},
+    {version: "0.8.13"},
     {version: "0.6.6"}],
     settings: {
       optimizer: {
