1671428d97c92c920299282296626a5ec1740fc8
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-05-08 21:06:51 -0400

    Update HighWei.js

--- test/integration/contracts/HighWei.js
+++ test/integration/contracts/HighWei.js
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.13;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
@@ -12,47 +12,48 @@ contract HighWei is ChainlinkClient, KeeperCompatibleInterface {
 
   uint public timeOpened; //Storage slot 0x00, 32/32, 32 bytes bytes.
   uint public tollPennies; //Storage slot 0x01, 32/32 bytes. MTA toll for Verrazano Bridge (Truck: Two Axle) in Pennies.
-  address public Owner; // Storage slot 0x02, 20/32 , 20 bytes.
-  uint96 public servoState; //Storage slot 0x02, 32/32 , 12 bytes.
+  address public immutable Owner; //Immutable variables do not take up a storage slot.
 
   event servoStateChange();
 
-    constructor() {
-      Owner = msg.sender;
-      setChainlinkToken(address(0x326C977E6efc84E512bB9C30f76E30c160eD06FB)); //MUMBAI ADDRESS FOR CHAINLINK API CONSUMER
-      priceFeed = AggregatorV3Interface(0xd0D5e3DB44DE05E9F294BB0a3bEEaF030DE24Ada); //MATIC/USD on Polygon Testnet Mumbai network.
-    }
+  constructor() {
+    Owner = msg.sender;
+    setChainlinkToken(address(0x326C977E6efc84E512bB9C30f76E30c160eD06FB)); //MUMBAI ADDRESS FOR CHAINLINK API CONSUMER
+    priceFeed = AggregatorV3Interface(0xd0D5e3DB44DE05E9F294BB0a3bEEaF030DE24Ada); //MATIC/USD on Polygon Testnet Mumbai network.
+  }
 
-    modifier onlyOwner() {
-        require(msg.sender == Owner, "ONLY_OWNER_FUNCTION.");
-        _;
-    }
+  modifier onlyOwner() {
+      require(msg.sender == Owner, "ONLY_OWNER_FUNCTION.");
+      _;
+  }
 
-     function feeInPenniesUSDinMatic() public view returns (uint) {
-        (uint80 roundID, int price, uint startedAt, uint timeStamp, uint80 answeredInRound) = priceFeed.latestRoundData();
-        return tollPennies*uint( (10**24) / price );
-    }
+  function feeInPenniesUSDinMatic() public view returns (uint) {
+      (uint80 roundID, int price, uint startedAt, uint timeStamp, uint80 answeredInRound) = priceFeed.latestRoundData();
+      return tollPennies*uint( (10**24) / price );
+  }
 
-    function openServoGate() public payable {
-        require(msg.value == feeInPenniesUSDinMatic() && msg.value != 0 , "MATCH_FEE_AND_FEE_NOT_ZERO_TO_OPEN.");
-        servoState = 1;
-        timeOpened = block.timestamp;
-        emit servoStateChange();
-    }
+  function openServoGate() public payable {
+      require(timeOpened == 0, "ALREADY_OPEN.");
+      require(msg.value == feeInPenniesUSDinMatic() && msg.value != 0 , "MATCH_FEE_AND_FEE_NOT_ZERO_TO_OPEN.");
+      timeOpened = block.timestamp;
+      emit servoStateChange();
+      payable(Owner).transfer(address(this).balance);
+  }
 
-    function closeServoGate() public onlyOwner { //Called by sensors (Ultrasonic or Keepers).
-        servoState = 0;
-        timeOpened = 0;
-        emit servoStateChange();
-    }
+  function closeServoGate() public onlyOwner { //Called by sensors (Ultrasonic or Keepers).
+      require(timeOpened != 0, "ALREADY_CLOSED.");
+      timeOpened = 0;
+      emit servoStateChange();
+  }
 
-    function checkUpkeep(bytes calldata) external override returns (bool upkeepNeeded, bytes memory) {
-        upkeepNeeded = oneBlockPassedSinceOpened();
-    }
+  function checkUpkeep(bytes calldata) external override returns (bool upkeepNeeded, bytes memory) {
+      upkeepNeeded = oneBlockPassedSinceOpened();
+  } 
 
-    function performUpkeep(bytes calldata) external override {
-        closeServoGate();
-    }
+  function performUpkeep(bytes calldata) external override { 
+      timeOpened = 0; //Can't call closeServoGate() directly because onlyOwner will block unique Keeper addresses. 
+      emit servoStateChange();
+  }
 
   function uintAdapterCall() public returns (bytes32 requestId) {
     Chainlink.Request memory request;
@@ -67,7 +68,7 @@ contract HighWei is ChainlinkClient, KeeperCompatibleInterface {
   }
 
   function oneBlockPassedSinceOpened() public view returns(bool) {
-    return (servoState == 1 && block.timestamp >= (timeOpened + 15));
+    return (timeOpened != 0 && block.timestamp >= (timeOpened + 15));
   }
 
 }
