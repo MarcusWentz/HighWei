aaa01bc419e935b34c05c94e70e13ef9f0c94edd
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2023-07-25 22:38:41 -0400

    Update AggregateScrapeTollMTAwithAdapter.js

--- scripts/AggregateScrapeTollMTAwithAdapter.js
+++ scripts/AggregateScrapeTollMTAwithAdapter.js
@@ -1,5 +1,5 @@
 const puppeteer = require('puppeteer');
-    (async () => { // REMOVE IN ADAPTER.JS
+(async () => { // REMOVE IN ADAPTER.JS
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
         await page.goto('https://new.mta.info/fares-and-tolls/bridges-and-tunnels/tolls-by-vehicle/trucks', { waitUntil: 'networkidle2' });
@@ -8,16 +8,14 @@ const puppeteer = require('puppeteer');
         const featureArticle2 = (await page.$x('/html/body/div[1]/div/div/section/div[4]/article/div/div/div/div/div/div/div/ul[3]/li[1]'))[0];
         const text2 = await page.evaluate(el => { return el.textContent}, featureArticle2);
         await page.goto('https://new.mta.info/fares-and-tolls/bridges-and-tunnels/tolls-by-vehicle/cars', { waitUntil: 'networkidle2' });
-        const featureArticle3 = (await page.$x('/html/body/div[1]/div/div/section/div[4]/article/div/div/div/div/div/ul[1]/li[1]'))[0];
+        const featureArticle3 = (await page.$x('/html/body/div[1]/div/div/section/div[4]/article/div/div/div[2]/div/div/div/div/ul[1]/li[1]'))[0];
         const text3 = await page.evaluate(el => { return el.textContent}, featureArticle3);
-        const featureArticle4 = (await page.$x('/html/body/div[1]/div/div/section/div[4]/article/div/div/div/div/div/ul[2]/li[4]'))[0];
+        const featureArticle4 = (await page.$x('/html/body/div[1]/div/div/section/div[4]/article/div/div/div[2]/div/div/div/div/ul[2]/li[4]'))[0];
         const text4 = await page.evaluate(el => { return el.textContent}, featureArticle4);
-        const featureArticle5 = (await page.$x('/html/body/div[1]/div/div/section/div[4]/article/div/div/div/div/div/ul[3]/li[1]'))[0];
+        const featureArticle5 = (await page.$x('/html/body/div[1]/div/div/section/div[4]/article/div/div/div[2]/div/div/div/div/ul[3]/li[1]'))[0];
         const text5 = await page.evaluate(el => { return el.textContent}, featureArticle5);
-        const featureArticle6 = (await page.$x('/html/body/div[1]/div/div/section/div[4]/article/div/div/div/div/div/ul[4]/li[1]'))[0];
+        const featureArticle6 = (await page.$x('/html/body/div[1]/div/div/section/div[4]/article/div/div/div[2]/div/div/div/div/ul[4]/li[1]'))[0];
         const text6 = await page.evaluate(el => { return el.textContent}, featureArticle6);
-        const featureArticle7 = (await page.$x('/html/body/div[1]/div/div/section/div[4]/article/div/div/div/div/div/ul[4]/li[4]'))[0];
-        const text7 = await page.evaluate(el => { return el.textContent}, featureArticle7);
         await browser.close();
         let priceArray = [];
         priceArray.push(text1.slice(10,text1.length)*100);
@@ -26,10 +24,9 @@ const puppeteer = require('puppeteer');
         priceArray.push(text4.slice(37,text4.length)*100);
         priceArray.push(text5.slice(11,text5.length)*100);
         priceArray.push(text6.slice(11,text6.length)*100);
-        priceArray.push(text7.slice(30,text7.length)*100);
         priceArray.sort(function(a, b){return a - b});
         console.log(priceArray); // REMOVE IN ADAPTER.JS
         let medianPrice = priceArray[Math.floor(priceArray.length/2)];
         console.log(Math.round(medianPrice)); // REMOVE IN ADAPTER.JS
         return BigInt(Math.round(medianPrice)); // BigInt to handle uint errors with Adapter.js
-    })(); // REMOVE IN ADAPTER.JS
+})(); // REMOVE IN ADAPTER.JS
